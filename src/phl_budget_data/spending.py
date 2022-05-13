2de5dc23f490f7b74822b35ac8fa784775378aae
Nick Hand <nicholas.adam.hand@gmail.com>
2022-05-13 17:10:07 -0400

    reorg for processed spending data

--- src/phl_budget_data/spending.py
+++ src/phl_budget_data/spending.py
@@ -1,60 +1,33 @@
-# from typing import Literal
+"""Load processed spending data from the data cache."""
 
-# import pandas as pd
-# from pydantic import validate_arguments
+from typing import Literal
 
-# from .utils import ETL_VERSION, optional_from_cache
+import pandas as pd
+from pydantic import validate_arguments
 
-# if ETL_VERSION:
-#     from .etl import spending
+from . import DATA_DIR
 
-# __all__ = ["load_budgeted_department_spending", "load_actual_department_spending"]
+CACHE_DIR = DATA_DIR / "processed" / "spending"
 
-
-# @optional_from_cache
-# @validate_arguments
-# def load_budgeted_department_spending(kind: Literal["adopted", "proposed"]):
-#     """
-#     Load budgeted spending by department and major class.
-
-#     Source: Annual Budget-in-Brief documents
-#     """
-
-#     # Get the dirname
-#     dirname = (
-#         spending.BudgetedDepartmentSpending.get_data_directory("processed")
-#         / kind
-#         / "budget"
-#     )
-
-#     # Glob the PDF files
-#     out = []
-#     for f in sorted(dirname.glob("*.csv")):
-#         out.append(pd.read_csv(f, dtype={"dept_code": str, "dept_major_code": str}))
-
-#     # Combine
-#     out = pd.concat(out, ignore_index=True)
-
-#     return out
+__all__ = ["load_budgeted_department_spending", "load_actual_department_spending"]
 
 
-# @optional_from_cache
-# def load_actual_department_spending():
-#     """
-#     Load actual spending by department and major class.
+@validate_arguments
+def load_budgeted_department_spending(
+    kind: Literal["adopted", "proposed"]
+) -> pd.DataFrame:
+    """
+    Load budgeted spending by department and major class.
 
-#     Source: Annual Budget-in-Brief documents
-#     """
-#     # Get the dirname
-#     dirname = spending.ActualDepartmentSpending.get_data_directory("processed")
+    Source: Annual Budget-in-Brief documents
+    """
+    return pd.read_csv(CACHE_DIR / f"budgeted-department-spending-{kind}.csv")
 
-#     # Glob the PDF files
-#     out = []
-#     for f in sorted(dirname.glob("*/actual/*.csv")):
-#         out.append(pd.read_csv(f, dtype={"dept_code": str, "dept_major_code": str}))
 
-#     out = pd.concat(out, ignore_index=True).drop_duplicates(
-#         subset=["dept_code", "fiscal_year"], keep="first"
-#     )
+def load_actual_department_spending() -> pd.DataFrame:
+    """
+    Load actual spending by department and major class.
 
-#     return out
+    Source: Annual Budget-in-Brief documents
+    """
+    return pd.read_csv(CACHE_DIR / f"actual-department-spending.csv")
